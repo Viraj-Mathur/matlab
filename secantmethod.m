de62982fad35a3d19bfae7db8b1867480f464a7a
Viraj Mathur <70805976+Viraj-Mathur@users.noreply.github.com>
2022-11-30 00:42:27 +0530

    Update secantmethod.m

--- secantmethod.m
+++ secantmethod.m
@@ -1,14 +1,16 @@
-clc
-clear all
-x0=input("Enter 1st initial guess");
-x1=input("Enter 2nd initial guess");
-t=input("Enter tolerance");
-f=@(x) x^2-17;
-error=1;
-while(error>t)
-    x2=(x0*f(x1)-x1*f(x0))/(f(x1)-f(x0)); %please write your formula correctly
-    error=abs(x2-x1);
-    x0=x1;
-    x1=x2;
+f=@ (x) x^2-17;
+x0=input('Enter x0');
+x1= input ('Enter x1');
+n=input('Enter iterations');
+tol=input('Enter tolerance');
+i=1;
+while (i<=n)
+x2=x1-((x1-x0) *f (x1) ) / (f(x1)-f(x0) ) ;
+if (abs (x1-x0) <tol)
+break;
 end
-fprintf("root is = %f ",x1);
+i=i+1;
+x0=x1;
+x1=x2;
+end
+display (x2);
